b195029731fd8bc3b922d158a4b7d4d2ddf65236
hle95 <hle99@horizon.csueastbay.edu>
2021-07-20 14:38:54 -0700

    Employee File Handler

--- Banking/src/EmployeeHandler.java
+++ Banking/src/EmployeeHandler.java
@@ -1,4 +1,39 @@
+import java.util.*;
+import java.io.*;
 
 public class EmployeeHandler {
-
+	public void parse() throws IOException {
+		String fileName = "Bank.csv";
+		List<String[]> data = new ArrayList<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+			String line = "";
+			while ((line = br.readLine()) != null) {
+				data.add(line.split(","));
+			}
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void save(Employee employee) throws IOException {
+		String fileName = "Bank.csv";
+		String name = employee.getName();
+		int ID = employee.getEmployeeID();
+		String userName = employee.getLoginusername();
+		String passWord = employee.getLoginpwd();
+		String[] data = {name, String.valueOf(ID), userName, passWord};
+		try (BufferedWriter br = new BufferedWriter(new FileWriter(fileName))) {
+			StringBuilder sb = new StringBuilder();
+			for (String item : data) {
+				sb.append(item);
+				sb.append(",");
+			}
+			br.write(sb.toString());
+			br.close();
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 }
