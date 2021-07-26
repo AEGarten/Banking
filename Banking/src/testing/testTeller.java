ed00a310bcdec1fd88465d9294d7ff0c4cf55ca4
hle95 <hle99@horizon.csueastbay.edu>
2021-07-25 17:20:14 -0700

    Update with a few test cases

--- Banking/src/testing/testTeller.java
+++ Banking/src/testing/testTeller.java
@@ -3,6 +3,7 @@ package testing;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.util.Date;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,9 +12,26 @@ import banking_dev.*;
 
 public class testTeller {
 	Teller teller;
+	Customer customer;
+	DataBase db;
+	LastTransaction lTrans;
+	Account acc;
 	@Before
 	public void initialize() throws ClassNotFoundException {
 		teller = new Teller();
+		
+		db = new DataBase();
+		customer = new Customer(444, "Sam Johnson", 
+                "secret", 2, 
+                0, 0, 
+                new Date(System.currentTimeMillis() - 7243600000L));
+		lTrans = new LastTransaction(new Money(0.01), new Money(6.00), "teller deposit");
+		acc = new Account( 34, AccountType.SAVINGS, 
+                new Money(1200.60), lTrans, 
+                new Date(System.currentTimeMillis() - 5243600000L), false, 
+                0);
+		customer.addAccount(acc);
+		db.addCustomer(customer);
 	}
 	@Test
 	public void testMoney() {
@@ -22,12 +40,23 @@ public class testTeller {
 	}
 	@Test
 	public void testAddAccount() throws ClassNotFoundException, IOException {
-		Enums enums = new Enums();
-		//Account account = new Account(0, enums.getAccountTypeChecking());
-		//assertEquals(true,teller.addAccount(account)); 
+		Account account = new Account(0, AccountType.CHECKING);
+		assertEquals(true,teller.addAccount(account)); 
 	}
 	@Test
 	public void testRemoveAccount() throws ClassNotFoundException, IOException {
-		
+		assertEquals(true,teller.removeAccount("34"));
+	}
+	@Test
+	public void testLogin() throws ClassNotFoundException, IOException {
+		Boolean[] test= {true, true};
+		Boolean[] data = teller.login("Login", "Password");
+		assertEquals(test, data); 
+	}
+	@Test
+	public void testCustLogin() throws ClassNotFoundException, IOException {
+		CustomerInfo custInfo = new CustomerInfo(true, customer);
+		CustomerInfo retrieve = teller.custLogin("secret", 44);
+		assertEquals(custInfo,retrieve);
 	}
 }
